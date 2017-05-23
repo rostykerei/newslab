fc2b2a47ad860996529f55c1d3b168c4641b1f14
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-05-23 17:43:42 +0200

    RssCrawlerDispatcherTest added

--- workers/worker-dispatcher/src/test/java/io/newslab/worker/dispatcher/RssCrawlerDispatcherTest.java
+++ workers/worker-dispatcher/src/test/java/io/newslab/worker/dispatcher/RssCrawlerDispatcherTest.java
@@ -1,25 +1,65 @@
 package io.newslab.worker.dispatcher;
 
+import io.newslab.messaging.rsscrawl.RssCrawlTaskMessage;
+import io.newslab.persistence.model.RssFeed;
+import io.newslab.persistence.repository.RssFeedRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = RssCrawlerDispatcher.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class RssCrawlerDispatcherTest {
 
-    @MockBean
-    RssCrawlerDispatcher rssCrawlerDispatcher;
+    @Mock
+    private RssFeedRepository rssFeedRepository;
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @InjectMocks
+    private RssCrawlerDispatcher rssCrawlerDispatcher;
 
     @Test
-    public void dispatchRssCrawl() throws Exception {
+    public void dispatchRssCrawl() {
+        rssCrawlerDispatcher.setDispatchFeedsNumber(5);
 
-//        given(rssCrawlerDispatcher.dispatchRssCrawl()).will
+        List<RssFeed> rssFeeds = new ArrayList<>();
+        RssFeed feed1 = new RssFeed();
+        feed1.setId(1);
+
+        RssFeed feed2 = new RssFeed();
+        feed2.setId(2);
+
+        rssFeeds.add(feed1);
+        rssFeeds.add(feed2);
+
+        when(rssFeedRepository.pollFeedsToProcess(5)).thenReturn(rssFeeds);
+
+        rssCrawlerDispatcher.dispatchRssCrawl();
+
+        verify(rssFeedRepository).pollFeedsToProcess(anyInt());
+        verify(rabbitTemplate, times(2)).convertAndSend(any(RssCrawlTaskMessage.class));
+    }
+
+    @Test
+    public void resetStuckFeeds() {
+        rssCrawlerDispatcher.setMaxProcessingTime(5000);
+
+        rssCrawlerDispatcher.resetStuckFeeds();
+
+        verify(rssFeedRepository, times(1)).resetIdleFeeds(5000);
     }
 
 }
