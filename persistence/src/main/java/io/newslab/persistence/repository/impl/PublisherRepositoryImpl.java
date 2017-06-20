62ce3c9f22c92c9a3e22fda1430612ba7003a6ed
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-06-20 20:36:03 +0200

    console init

--- persistence/src/main/java/io/newslab/persistence/repository/impl/PublisherRepositoryImpl.java
+++ persistence/src/main/java/io/newslab/persistence/repository/impl/PublisherRepositoryImpl.java
@@ -3,11 +3,13 @@ package io.newslab.persistence.repository.impl;
 import io.newslab.persistence.model.Publisher;
 import io.newslab.persistence.repository.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.Optional;
 
+@Repository
 public class PublisherRepositoryImpl extends BaseRepositoryImpl<Publisher, Integer> implements PublisherRepository {
 
     @Autowired
