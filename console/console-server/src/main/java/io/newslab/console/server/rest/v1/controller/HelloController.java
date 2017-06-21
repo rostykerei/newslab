1d04e68af35d1f204d02e9a50f0e85779471234b
Rosty Kerei <rosty.kerei@gmail.com>
2017-06-21 22:22:54 +0200

    console swagger

--- console/console-server/src/main/java/io/newslab/console/server/rest/v1/controller/HelloController.java
+++ console/console-server/src/main/java/io/newslab/console/server/rest/v1/controller/HelloController.java
@@ -1,14 +1,19 @@
 package io.newslab.console.server.rest.v1.controller;
 
 import io.newslab.console.server.rest.v1.response.HelloResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "/hello", description = "Test API")
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    @ApiOperation(value = "Returns test Hello response")
+    @RequestMapping(method = RequestMethod.GET)
     public HelloResponse message() {
         return new HelloResponse(123, "Hello World");
     }
