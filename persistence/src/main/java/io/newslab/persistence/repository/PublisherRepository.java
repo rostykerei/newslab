9bdb1b782d3eb9e1ed8d00d3d11ea4efa10f9340
Rosty Kerei <rosty.kerei@gmail.com>
2017-06-26 22:05:17 +0200

    pagination effort

--- persistence/src/main/java/io/newslab/persistence/repository/PublisherRepository.java
+++ persistence/src/main/java/io/newslab/persistence/repository/PublisherRepository.java
@@ -1,10 +1,11 @@
 package io.newslab.persistence.repository;
 
 import io.newslab.persistence.model.Publisher;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface PublisherRepository extends BaseRepository<Publisher, Integer> {
+public interface PublisherRepository extends PagingAndSortingRepository<Publisher, Integer> {
 
     Optional<Publisher> findByCanonicalName(String cn);
 
